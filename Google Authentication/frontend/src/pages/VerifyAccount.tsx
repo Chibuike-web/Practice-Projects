4060d59b6a14917cc3770d254b3e99cbf4c46d51
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-05-31 04:54:52 +0100

    updates

--- Google Authentication/frontend/src/pages/VerifyAccount.tsx
+++ Google Authentication/frontend/src/pages/VerifyAccount.tsx
@@ -1,11 +1,12 @@
 import { FormEvent, useState } from "react";
 import Button from "../components/Button";
-import { VerifyIcon } from "../components/Icons";
+import { VerifyIcon, WarningIcon } from "../components/Icons";
 import { twMerge } from "tailwind-merge";
 import { Check } from "lucide-react";
 
 export default function VerifyAccount() {
 	const [selectedMethod, setSelectedMethod] = useState<"email" | "phone" | null>(null);
+	const [phoneNumber, setPhoneNumber] = useState<string>("");
 
 	const handleSelect = (e: FormEvent, method: "email" | "phone") => {
 		e.preventDefault();
@@ -17,6 +18,29 @@ export default function VerifyAccount() {
 			"w-full h-12 p-[14px] rounded-md bg-very-light-gray mt-4 flex items-center justify-start",
 			isSelected && "border border-primary justify-between"
 		);
+
+	const renderInfoMessage = () => {
+		if (selectedMethod === "email") {
+			return (
+				<div className="mt-4 flex items-start text-sm text-light-gray gap-2">
+					<WarningIcon />
+					<span>A 6-digit verification code will be sent to the email you provided.</span>
+				</div>
+			);
+		}
+		if (selectedMethod === "phone") {
+			return (
+				<div className="mt-4 flex items-start text-sm text-light-gray gap-2">
+					<WarningIcon />
+					<span>
+						You’ll receive a 6-digit verification code to the phone number you provided. Text rates
+						may apply.
+					</span>
+				</div>
+			);
+		}
+		return null;
+	};
 
 	return (
 		<main className="grid place-items-center h-screen px-4">
@@ -48,6 +72,28 @@ export default function VerifyAccount() {
 						<span>Phone number</span>
 						{selectedMethod === "phone" && <Check className="text-primary" />}
 					</Button>
+
+					{selectedMethod === "phone" && (
+						<fieldset className="flex flex-col gap-2 mt-10">
+							<label htmlFor="phone" className="text-sm text-dark-gray font-medium">
+								Phone number
+							</label>
+							<input
+								id="phone"
+								type="tel"
+								placeholder="Enter your phone number"
+								value={phoneNumber}
+								onChange={(e) => setPhoneNumber(e.target.value)}
+								required
+								className={twMerge(
+									"text-sm px-4 py-3 border border-input-stroke",
+									"bg-very-light-gray rounded-md w-full placeholder:text-light-gray focus:border-primary"
+								)}
+							/>
+						</fieldset>
+					)}
+
+					{renderInfoMessage()}
 				</div>
 
 				<Button
