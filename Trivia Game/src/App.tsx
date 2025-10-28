5bb15a69bc7d422d283c2a19c6282a08564362c0
Chibuike Maduabuchi <chibuikemaduabuchi2023@gmail.com>
2025-10-28 03:37:18 +0100

    trivia-game

---
+++ Trivia Game/src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+
+export default function App() {
+	return (
+		<Router>
+			<Routes>
+				<Route />
+			</Routes>
+		</Router>
+	);
+}
